9d718ecd8c275826ea11ce13d7cb3c098ac9bf4b
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2022-01-08 17:10:50 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,3 +43,9 @@ tasks.withType<KotlinCompile> {
 tasks.withType<Test> {
 	useJUnitPlatform()
 }
+
+allOpen {
+	annotation("javax.persistence.Entity")
+	annotation("javax.persistence.MappedSuperclass")
+	annotation("javax.persistence.Embeddable")
+}
