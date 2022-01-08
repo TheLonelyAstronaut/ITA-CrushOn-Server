0f9725e2e5d542af07d40ce3d2a58c747d4d7a8a
孤独な 宇宙飛行士 <50376577+TheLonelyAstronaut@users.noreply.github.com>
2022-01-08 16:25:47 +0300

    init

---
+++ src/main/kotlin/com/itechart/crushon/CrushonApplication.kt
@@ -0,0 +1,11 @@
+package com.itechart.crushon
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CrushonApplication
+
+fun main(args: Array<String>) {
+	runApplication<CrushonApplication>(*args)
+}
---
+++ src/test/kotlin/com/itechart/crushon/CrushonApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.itechart.crushon
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CrushonApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
